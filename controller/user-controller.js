4880238c96ec41589e73c61f80869a6d160e7751
karan jain <karanjain2212@gmail.com>
2023-09-17 10:46:39 +0530

    edit and delete routes

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import User from "../schema/user-schema.js";
 
 export const addUser = async (request, response) => {
@@ -17,5 +18,46 @@ export const addUser = async (request, response) => {
     console.log("success, user created in database ");
   } catch (error) {
     response.status(409).json({ message: error.message });
+  }
+};
+
+export const getUsers = async (request, response) => {
+  try {
+    const users = await User.find({});
+    response.status(200).json(users); //this sends to fronend
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
+
+export const getUserById = async (request, response) => {
+  //   console.log(request.params.id);
+  try {
+    const user = await User.find({ _id: request.params.id });
+    // const user = awit User.findById(request.params.id)
+    response.status(200).json(user); //this sends to fronend
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
+
+export const editUser = async (request, response) => {
+  let user = request.body;
+  const editUser = new User(user);
+
+  try {
+    await User.updateOne({ _id: request.params.id }, editUser);
+    response.status(201).json(editUser);
+  } catch (error) {
+    response.status(409).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (request, response) => {
+  try {
+    await User.deleteOne({ _id: request.params.id });
+    response.status(200).json({ message: "user deleted successfully" });
+  } catch (error) {
+    response.status(409).json({ message: "something went wrong" });
   }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,18 @@
 import express from "express";
 import {
-  //   getUsers,
+  getUsers,
   addUser,
-  //   getUserById,
-  //   editUser,
-  //   deleteUser,
+  getUserById,
+  editUser,
+  deleteUser,
 } from "../controller/user-controller.js";
 
 const router = express.Router();
 
-// router.get("/", getUsers);
 router.post("/add", addUser);
-// router.get("/:id", getUserById);
-// router.put("/:id", editUser);
-// router.delete("/:id", deleteUser);
+router.get("/all", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", editUser);
+router.delete("/:id", deleteUser);
 
 export default router;
